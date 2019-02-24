3076427a7cecea912e34fe90b0ccb3dc6dba65b9
Tiago Cunha <tcunha_lp@hotmail.com>
2019-02-24 16:41:07 +0000

    real final commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quotes = Quote.create ([{body: 'Excellence is not a singular act but a habit. You are what you do repeatedly.', author: 'Shaquille O’Neal'}, {body: 'It’s hard to beat a person who never gives up.', author: 'Babe Ruth'}, {body: 'You can’t put a limit on anything. The more you dream, the farther you get.', author: 'Michael Phelps'}, {body: 'I hated every minute of training, but I said, ‘Don’t quit. Suffer now and live the rest of your life as a champion.', author: 'Muhammad Ali'}, {body: 'Excellence is the gradual result of always striving to do better.', author: 'Pat Riley'}, {body: 'If it doesn’t challenge you, it won’t change you.', author: 'Fred Devito'}])
